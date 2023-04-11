abb8f2c6df1a99907ff2518008dc3983784e6ce6
Caick Andrade <caickandrade0@gmail.com>
2023-04-10 23:59:40 -0300

    add: round selection area

--- src/components/pages/Counter/TeamInfo/RoundPoints.jsx
+++ src/components/pages/Counter/TeamInfo/RoundPoints.jsx
@@ -1,12 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import { POINTS_DIC } from '../../../../utils/counterConstants'
 
 const RoundPoints = ({ onSelectRoundPoints }) => {
-  return (
-    <div>
 
+  const [selectedRound, setSelectedRound] = useState('comum');
+
+  return (
+    <div className='flex flex-wrap items-center justify-center gap-2 '>
+      <Button title="comum" selected={selectedRound === 'comum'} onSelect={setSelectedRound} />
+      <Button title="truco" selected={selectedRound === 'truco'} onSelect={setSelectedRound} />
+      <Button title="duque" selected={selectedRound === 'duque'} onSelect={setSelectedRound} />
+      <Button title="bispo" selected={selectedRound === 'bispo'} onSelect={setSelectedRound} />
     </div>
+  )
+}
+
+const Button = ({ title, selected, onSelect }) => {
+  return(
+    <button
+      className='flex items-center justify-center p-2 min-w-[40%] border-2 border-gray-500 rounded-lg'
+      style={{ backgroundColor: selected ? 'red' : 'transparent' }}
+      onClick={() => onSelect(title)}
+    >
+      {title}
+    </button>
   )
 }
 
