4edf403617aebcceb98be01df67c58c2cc9cddf0
Caick Andrade <caickandrade0@gmail.com>
2023-04-25 18:05:05 -0300

    add: rocks first layer hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,30 @@
 import { useState } from 'react'
 import './App.css'
+import Plx from 'react-plx';
 
 import Counter from './components/pages/Counter';
+import Rocks1 from './components/elements/Rocks1';
+
+const parallaxData = [
+  {
+    start: 0,
+    end: 500,
+    properties: [
+      {
+        startValue: 1,
+        endValue: 3,
+        property: "scale",
+      },
+    ],
+  },
+];
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="flex ">
-      <Counter />
+    <div className='relative h-screen w-screen'>
+      <Rocks1 />
     </div>
   )
 }
---
+++ src/components/elements/Rocks1.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import rocks from '../../assets/rocks-1.png';
+
+const Rocks1 = () => {
+  return (
+    <img 
+        src={rocks}
+        alt="rocks 1"
+    />
+  )
+}
+
+export default Rocks1
