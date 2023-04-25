5d8188eeea0b8df805c1edd41ffe13c7689a1e35
Caick Andrade <caickandrade0@gmail.com>
2023-04-25 18:21:16 -0300

    add: rocks second layer hero

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Plx from 'react-plx';
 
 import Counter from './components/pages/Counter';
 import Rocks1 from './components/elements/Rocks1';
+import Rocks2 from './components/elements/Rocks2';
 
 const parallaxData = [
   {
@@ -12,7 +13,7 @@ const parallaxData = [
     properties: [
       {
         startValue: 1,
-        endValue: 3,
+        endValue: 2,
         property: "scale",
       },
     ],
@@ -23,8 +24,14 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className='relative h-screen w-screen'>
-      <Rocks1 />
+    <div className='relative h-screen w-screen max-h-screen max-w-screen'>
+      <Plx className='absolute w-full h-full z-50' parallaxData={parallaxData}>
+        <Rocks1 />
+      </Plx>
+
+      <Plx className='absolute w-full h-full z-40' parallaxData={parallaxData}>
+        <Rocks2 />
+      </Plx>
     </div>
   )
 }
--- src/components/elements/Rocks2.jsx
+++ src/components/elements/Rocks2.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 
-import rocks from '../../assets/rocks-1.png';
+import rocks from '../../assets/rocks-2.png';
 
-const Rocks1 = () => {
+const Rocks2 = () => {
   return (
     <img 
         src={rocks}
-        alt="rocks 1"
+        alt="rocks 2"
         className='h-full w-full'
     />
   )
 }
 
-export default Rocks1
+export default Rocks2
